43b9a41d0714423d7581dfbf9008325a47bea25e
Aina Gbolahan <ainagbolahan100@gmail.com>
2023-02-14 09:59:45 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,7 @@ const { login, register } = require("../controllers/auth");
 const router = express.Router();
 
 
-router.post("/register", register);
+router.route("/register").post( register);
 router.post("/login", login);
+
+module.exports = router;
