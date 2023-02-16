e5f5088abcfedbaaaf593ee4f244776b8f828383
Aina Gbolahan <ainagbolahan100@gmail.com>
2023-02-16 10:25:00 +0100

    Added login

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,13 +10,32 @@ const register = async (req, res) => {
 	// const tempuser = {name,email,password:hashedPassword}
 
 	const user = await User.create({ ...req.body })
-   const token =  user.generateToken()
-    // console.log(token);
+	const token = user.generateToken()
+	// console.log(token);
 
 	res.status(StatusCodes.CREATED).json({ user, token })
 }
 
-const login = async (req, res) => {}
+const login = async (req, res) => {
+	const user = await User.findOne({ email: req.body.email })
+	if (!user) {
+		return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Invalid email' })
+	}
+	const comparePassword = user.comparePassword(req.body.password)
+	if (!comparePassword) {
+		return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Invalid password' })
+	}
+
+	const token = user.generateToken()
+	return res.status(StatusCodes.OK).json({
+		token,
+		user: {
+			name: user.name,
+			_id: user._id,
+			email: user.email,
+		},
+	})
+}
 
 module.exports = {
 	register,
--- models/User.js
+++ models/User.js
@@ -1,49 +1,54 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const JWT =  require("jsonwebtoken");
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const JWT = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please provide name"],
-        minlength: 3,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        required: [true, "Please provide email"],
-        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please provide a valid email"],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, "Please provide password"],
-        minlength: 6,
-       
-    },
-});
+	name: {
+		type: String,
+		required: [true, 'Please provide name'],
+		minlength: 3,
+		maxlength: 50,
+	},
+	email: {
+		type: String,
+		required: [true, 'Please provide email'],
+		match: [
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			'Please provide a valid email',
+		],
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: [true, 'Please provide password'],
+		minlength: 6,
+	},
+})
 
-userSchema.pre("save", async function (next) {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    this.password = hashedPassword
-    next();
-  
-});
+userSchema.pre('save', async function (next) {
+	const salt = await bcrypt.genSalt(10)
+	const hashedPassword = await bcrypt.hash(this.password, salt)
+	this.password = hashedPassword
+	next()
+})
 
-userSchema.method("generateToken", function () {
-    const token = JWT.sign(
-        {
-            userId: this._id,
-            email: this.email,
-            name: this.name,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: '30d' }
-    );
-    return token
-});
+userSchema.method('generateToken', function () {
+	const token = JWT.sign(
+		{
+			userId: this._id,
+			email: this.email,
+			name: this.name,
+		},
+		process.env.JWT_SECRET,
+		{ expiresIn: '30d' }
+	)
+	return token
+})
 
-const User = mongoose.model("User", userSchema);
+userSchema.method('comparePassword', function (password) {
+	return bcrypt.compareSync(password, this.password)
+})
 
-module.exports = { User };
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
