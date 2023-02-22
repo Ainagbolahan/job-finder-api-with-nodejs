7ac99daee9b7a7d2d76038866b7849ff4fd9a239
Aina Gbolahan <ainagbolahan100@gmail.com>
2023-02-22 08:58:10 +0100

    More update in Job controller(update job,find one)

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,37 +1,58 @@
-const { StatusCodes } = require("http-status-codes")
-const { BadRequestError, NotFoundError } = require("../errors")
-const { Job } = require("../models/Job")
-
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+const { Job } = require('../models/Job')
 
 const getAllJobs = async (req, res) => {
-    
+	const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+	return res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
-    
+	const {
+		user: { userId },
+		params: { id: jobId },
+	} = req
+
+	const jobs = await Job.find({ createdBy: userId, _id: jobId })
+	if (!jobs) {
+		throw new NotFoundError('No job found')
+	}
+	return res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const createJob = async (req, res) => {
-    req.body.createdBy = req.user.userId
-    const job = await Job.create(req.body);
-    return res.status(StatusCodes.CREATED).json(job)
-    
+	req.body.createdBy = req.user.userId
+	const job = await Job.create(req.body)
+	return res.status(StatusCodes.CREATED).json(job)
 }
 
 const updateJob = async (req, res) => {
-    
+	const {
+		user: { userId },
+		params: { id: jobId },
+		body: { company, position },
+	} = req
+
+	if (company === '' || position === '') {
+		throw new BadRequestError('Company and position are required')
+	}
+
+	const jobs = await Job.findOneAndUpdate({ _id: userId, createdBy: userId }, req.body, {
+		new: true,
+		runValidators: true,
+	})
+	if (!jobs) {
+		throw new NotFoundError('No job found')
+	}
+	return res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
-const deleteJob = async (req, res) => {
-    
-}
-
+const deleteJob = async (req, res) => {}
 
 module.exports = {
-    getAllJobs,
-    getJob,
-    createJob,
-    updateJob,
-    deleteJob
-    
-};
+	getAllJobs,
+	getJob,
+	createJob,
+	updateJob,
+	deleteJob,
+}
