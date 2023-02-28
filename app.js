b9d86c3b37f9cdfc3e065620513a7c8cca2579f8
Aina Gbolahan <ainagbolahan100@gmail.com>
2023-02-28 09:17:53 +0100

    Update security packages

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+
+// Extra security
+const helmet = require("helmet");
+const cors = require('cors');
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
+
+
+
 // ConnecDb
 
 const connectDB = require("./db/connect")
@@ -15,9 +25,21 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const { verifyToken } = require('./middleware/authentication');
 
-
+app.set("trust proxy", 1);
 app.use(express.json());
 // extra packages
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  // burst: 20,
+  methods: ['GET', 'POST', 'PATCH', 'PUT']
+  }));
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
 
 // routes
 
